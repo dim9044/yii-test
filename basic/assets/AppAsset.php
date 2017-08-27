d71bd4677ff3ab8e395441b320acdcc2fdd68d4e
DIMA_N <dim9044@yandex.ru>
2017-08-27 23:01:25 +0300

    site-1

--- basic/assets/AppAsset.php
+++ basic/assets/AppAsset.php
@@ -18,12 +18,41 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+//        'css/site.css',
+        'css/animate.css',
+        'css/bootstrap.min.css',
+        'css/custom-blue.css',
+        'css/custom-green.css',
+        'css/custom-purple.css',
+        'css/custom-red.css',
+        'css/ionicons.min.css',
+        'css/magnific-popup.css',
+        'css/owl.carousel.css',
+        'css/owl.transitions.css',
     ];
     public $js = [
+        'js/bootstrap.js',
+        'js/custom-fun.js',
+        'js/enquire.js',
+        'js/jquery.easing.1.3.js',
+        'js/jquery.magnific-popup.js',
+        'js/jquery.min.js',
+        'js/jquery.nicescroll.js',
+        'js/jquery.nicescroll.plus.js',
+        'js/jquery.pickmeup.js',
+        'js/jquery.scrollUp.js',
+        'js/jquery.simpleWeather.min.js',
+        'js/jquery.ticker.js',
+        'js/masonry.pkgd.js',
+        'js/moment.min.js',
+        'js/owl.carousel.js',
+        'js/style-switcher.js',
+        'js/wow.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
     ];
+
+
 }
